e1763c2cc6325ef5aba5d8b2884299515c2afa25
LA289DM <LA289DM@XE2268974W2.ey.net>
2018-11-02 15:18:00 +0100

    next commit

--- src/main/java/com/springframework/Employee.java
+++ src/main/java/com/springframework/Employee.java
@@ -4,7 +4,14 @@ public class Employee {
 
 	private int id;
 	private String name;
+	private Address address;
 
+	public Employee(int id, String name, Address address) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -20,4 +27,17 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}		
 }
--- src/test/java/com/springframework/test/Test.java
+++ src/test/java/com/springframework/test/Test.java
@@ -9,9 +9,12 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
 		Employee employee01 = (Employee) context.getBean("employee01");
-		System.out.println("Employee: ID = " + employee01.getId() + ", Name = " + employee01.getName());
+		System.out.println("Employee: ID = " + employee01.getId() + ", Name = " + employee01.getName() + ", hash = " + employee01.hashCode());
 		
 		Employee employee02 = (Employee) context.getBean("employee02");
-		System.out.println("Employee: ID = " + employee02.getId() + ", Name = " + employee02.getName());
+		System.out.println("Employee: ID = " + employee02.getId() + ", Name = " + employee02.getName() + ", hash = " + employee02.hashCode());
+		
+		Employee employee03 = (Employee) context.getBean("employee01");
+		System.out.println("Employee: ID = " + employee03.getId() + ", Name = " + employee03.getName() + ", hash = " + employee03.hashCode());
 	}
 }
